31482af1223ba736f46d78c4cc58c28aede803b1
ThePixeLe <lsoilihi@arfp.asso.fr>
2026-01-21 14:10:21 +0100

    exercice graph

---
+++ exercice/ExoPoint.java
@@ -0,0 +1,45 @@
+package exercice;
+
+public class ExoPoint {
+    // Attributs de la classe
+    private double x = 0;
+    private double y = 0;
+
+
+    // Constructeur
+    public ExoPoint() {
+    }
+
+    
+    public ExoPoint(double x, double y) {
+        this.x = x ;    // this.x fait référence à l'attribut de l'objet, x au paramètre
+        this.y = y ;    // this.y fait référence à l'attribut de l'objet, y au paramètre
+
+
+    
+        public double getX() {
+            return x;
+        }
+
+        public double getY() {
+            return y;
+        }
+
+        public void permuter (unPoint Point) {
+         
+        }
+
+        public void Point() {
+
+        }
+
+        public void Point (_X :double, _Y :double) {
+
+        }
+
+        
+    }
+    
+}
+
+// ExoPoint.java
