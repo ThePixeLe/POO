98282ccf5e1c2f93d52659f150deaad386dd0c86
ThePixeLe <lsoilihi@arfp.asso.fr>
2026-02-06 09:57:41 +0100

    fraction

---
+++ exercice/compte/Fraction.java
@@ -0,0 +1,100 @@
+package System;
+
+/**
+ * @version 1.0
+ * @created 06-févr.-2026 09:52:58
+ */
+public class Fraction {
+
+	private float Numerateur;
+	private float Denominateur;
+
+	public Fraction(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	private int GetPgcd(){
+		return 0;
+	}
+
+	private Reduire(){
+
+	}
+
+	/**
+	 * 
+	 * @param unefraction
+	 */
+	public boolean Superieur(Fraction unefraction){
+		return false;
+	}
+
+	/**
+	 * 
+	 * @param unefraction
+	 */
+	public boolean Inferieur(Fraction unefraction){
+		return false;
+	}
+
+	/**
+	 * 
+	 * @param unefraction
+	 */
+	public boolean Egal(Fraction unefraction){
+		return false;
+	}
+
+	/**
+	 * 
+	 * @param unefraction
+	 */
+	public Fraction Somme(Fraction unefraction){
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param unefraction
+	 */
+	public Fraction Difference(Fraction unefraction){
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param unefraction
+	 */
+	public Fraction Produit(Fraction unefraction){
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param unefraction
+	 */
+	public Fraction Quotient(Fraction unefraction){
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param exposant
+	 */
+	public Fraction Puissance(int exposant){
+		return null;
+	}
+
+	public Oppose(){
+
+	}
+
+	public Inverse(){
+
+	}
+
+}
